50dadae2f598f31c6d2f6177b181188b0c4e3099
Daniel Abraham <aeroquix@gmail.com>
2026-09-15 13:23:18 -0700

    fix for review comment

--- cmd/versi/senders.go
+++ cmd/versi/senders.go
@@ -57,9 +57,6 @@ func initSenders(entireCfg map[string]any) (map[string]config.Sender, bool) {
 				)
 				ok = false
 			}
-		default:
-			slog.Error("unrecognized sender type", slog.String("name", name), slog.String("type", baseType))
-			ok = false
 		}
 	}
 
