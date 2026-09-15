d3b23c778645e2da0d509c50b7051a78bb8bdc86
Daniel Abraham <aeroquix@gmail.com>
2026-09-15 13:25:26 -0700

    tiny safety tweak

--- cmd/versi/senders.go
+++ cmd/versi/senders.go
@@ -57,6 +57,9 @@ func initSenders(entireCfg map[string]any) (map[string]config.Sender, bool) {
 				)
 				ok = false
 			}
+		default:
+			slog.Error("unrecognized sender type", slog.String("name", name), slog.String("type", baseType))
+			ok = false
 		}
 	}
 
